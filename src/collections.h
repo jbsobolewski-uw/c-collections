fc56a79617082a8a1533f35930d19174c8cc7a4c
js469693 <jb.sobolewsk@student.uw.edu.pl>
2026-04-13 22:40:16 +0200

    Add collections package - aggregated library

---
+++ src/collections.h
@@ -0,0 +1,13 @@
+//
+// Created by jakub on 4/13/26.
+//
+
+#ifndef C_COLLECTIONS_COLLECTIONS_H
+#define C_COLLECTIONS_COLLECTIONS_H
+
+#include "bst/bst.h"
+#include "queue/queue.h"
+#include "slist/slist.h"
+#include "stack/stack.h"
+
+#endif //C_COLLECTIONS_COLLECTIONS_H
